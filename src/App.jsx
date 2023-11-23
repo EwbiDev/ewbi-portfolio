41b6e423658c6aa2a1dc7c90b2b20d6fd3433893
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-23 11:42:17 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="p-4 font-cursive text-9xl">Ewbi</h1>
+      <p className="font-serif">Eduard Bissell - Portfolio</p>
+    </>
+  );
+}
+
+export default App;
