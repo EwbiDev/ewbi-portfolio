691cfba03f3e825199327c44baca3c6701aea63f
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-23 12:28:27 +0000

    add: NavBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import Logo from "./components/Logo";
+import NavBar from "./components/NavBar";
 
 export default function Header() {
   return (
-    <header >
+    <header>
       <Logo />
+      <NavBar />
     </header>
   );
 }
