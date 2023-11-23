07481c2aca0b0a5f03744612f79f9e0f9320193b
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-23 15:41:25 +0000

    add: carousel logic to Title

--- src/commonComponents/Title.jsx
+++ src/commonComponents/Title.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Title() {
+  const [carouselArray, setCarouselArray] = useState([
+    "Eduard Bissell",
+    "Software Developer",
+  ]);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  const carouselText = carouselArray[carouselIndex];
+
+  useEffect(() => {
+    function pickCarouselItem() {
+      setCarouselIndex((carouselIndex + 1) % carouselArray.length);
+    }
+
+    setTimeout(pickCarouselItem, 5000);
+  }, [carouselIndex, carouselArray]);
+
   return (
     <div>
       <h1>I am</h1>
-      <h2>Eduard Bissell</h2>
+      <h2 className="carousel">{carouselText}</h2>
     </div>
   );
 }
