7822ca7cb9d0f8bd766a4a5d4581c5fc3ae4b147
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-23 14:55:13 +0000

    add: Content positioning

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
+    <div className="h-screen w-screen flex flex-col">
       <Header />
       <Content />
-    </>
+    </div>
   );
 }
 
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,3 +1,3 @@
 export default function Content() {
-  return <div className="content container bg-red-300"></div>;
+  return <div className="content container h-full w-full bg-red-300 place-self-center"></div>;
 }
