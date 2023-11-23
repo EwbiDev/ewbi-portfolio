c228308d11f12796719e94b16d3762ee8e519383
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-23 15:04:41 +0000

    populate: Content with Landing

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,3 +1,9 @@
+import Landing from "./components/Landing";
+
 export default function Content() {
-  return <div className="content container h-full w-full place-self-center"></div>;
+  return (
+    <div className="content container h-full w-full place-self-center">
+      <Landing />
+    </div>
+  );
 }
