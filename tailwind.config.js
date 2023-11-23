67cea441642e1175138ad3df8734019a44be2dd2
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-23 12:35:42 +0000

    add: theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ewbi-gold": "#EDB155",
+        "ewbi-red": "#46171C",
+        "ewbi-page": "#FAF0E0"
+      }
+    },
     fontFamily: {
       cursive: "Parisienne, cursive",
       serif: "Rokkitt, serif",
