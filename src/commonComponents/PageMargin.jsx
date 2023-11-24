747c385fa3ab415a3e9829dd2d600128a3e5237f
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-24 11:26:05 +0000

    add: conditional Portrait render

--- src/commonComponents/PageMargin.jsx
+++ src/commonComponents/PageMargin.jsx
@@ -1,9 +1,9 @@
 import Portrait from "./Portrait";
 
-export default function PageMargin() {
+export default function PageMargin({showImage}) {
   return (
     <div className="bg-green-200 sm:basis-1/6 xl:basis-1/5 2xl:basis-1/4">
-      <Portrait />
+      {showImage && <Portrait />}
     </div>
   );
 }
