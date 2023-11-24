178de04a08f54fee32c6309563e7518cc13bbda1
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-24 18:33:56 +0000

    add: glitch effect

--- src/commonComponents/TextCarousel.jsx
+++ src/commonComponents/TextCarousel.jsx
@@ -1,15 +1,37 @@
 import { useEffect, useState } from "react";
 
 export default function TextCarousel({ inputTextArray }) {
-  const [activeItem, setActiveItem] = useState(0);
+  const [activeItemIndex, setActiveItem] = useState(0);
+  const [glitch, setGlitch] = useState(false);
+  const [glitchIndex, setGlitchIndex] = useState(0);
+
+  const activeText = inputTextArray[activeItemIndex];
+  const glitchColors = ["text-red-400"];
+
+  useEffect(() => {
+    function randGlitchIdx() {
+      setGlitchIndex(Math.floor(Math.random() * activeText.length));
+    }
+
+    if (glitch) {
+      setTimeout(randGlitchIdx, 50);
+    }
+  }, [glitchIndex, glitch]);
 
   useEffect(() => {
     function nextItem() {
-      setActiveItem((activeItem + 1) % inputTextArray.length);
+      setActiveItem((activeItemIndex + 1) % inputTextArray.length);
+
+      const glitchChance = Math.random() * 10;
+      if (glitchChance < 10) {
+        setGlitch(true);
+      } else {
+        setGlitch(false);
+      }
     }
 
     setTimeout(nextItem, 5000);
-  }, [activeItem, inputTextArray]);
+  }, [activeItemIndex, inputTextArray]);
 
   return (
     <div className="text-carousel flex overflow-hidden">
@@ -17,10 +39,19 @@ export default function TextCarousel({ inputTextArray }) {
         <div
           key={`carousel-${index}`}
           className={`absolute w-full transition duration-1000 ease-in-out ${
-            activeItem === index ? "opacity-100" : "translate-x-1/4 opacity-0"
+            activeItemIndex === index
+              ? "opacity-100"
+              : "translate-x-1/4 opacity-0"
           }`}
         >
-          {input}
+          {input.split("").map((char, idx) => (
+            <span
+              key={`${activeText}-${idx}`}
+              className={`${glitch && idx === glitchIndex && glitchColors[0]}`}
+            >
+              {char}
+            </span>
+          ))}
         </div>
       ))}
     </div>
