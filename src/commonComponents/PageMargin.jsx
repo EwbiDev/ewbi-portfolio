d619b8681c0e2ed22c176d69c39f3a32d94bc148
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-24 10:34:40 +0000

    populate: PageMargin with Portrait

--- src/commonComponents/PageMargin.jsx
+++ src/commonComponents/PageMargin.jsx
@@ -1,3 +1,9 @@
+import Portrait from "./Portrait";
+
 export default function PageMargin() {
-  return <div className="sm:basis-1/6 xl:basis-1/5 2xl:basis-1/4 bg-green-200"></div>;
+  return (
+    <div className="bg-green-200 sm:basis-1/6 xl:basis-1/5 2xl:basis-1/4">
+      <Portrait />
+    </div>
+  );
 }
