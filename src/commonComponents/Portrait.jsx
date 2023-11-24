5f3484f976c7f5e660d5ffe91dbb6884a53baa81
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-24 09:54:26 +0000

    add: Portrait

---
+++ src/commonComponents/Portrait.jsx
@@ -0,0 +1,5 @@
+import landingImg from "@assets/portraits/landing.png"
+
+export default function Portrait() {
+  return <img className="absolute h-full bottom-0 right-0" src={landingImg} alt="landing image" />
+}
