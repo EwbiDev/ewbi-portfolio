046733a8acb3ac60d556464397dd8d4d7764202d
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-24 10:33:30 +0000

    remove: Portrait classes

--- src/commonComponents/Portrait.jsx
+++ src/commonComponents/Portrait.jsx
@@ -1,5 +1,5 @@
 import landingImg from "@assets/portraits/landing.png"
 
 export default function Portrait() {
-  return <img className="absolute h-full bottom-0 right-0" src={landingImg} alt="landing image" />
+  return <img className="" src={landingImg} alt="landing image" />
 }
