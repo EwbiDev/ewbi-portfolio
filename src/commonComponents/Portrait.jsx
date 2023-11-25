0c92062de7e46d7236bf3a36d07944ec7f04491f
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-25 11:19:12 +0000

    add: Portrait red filter

--- src/commonComponents/Portrait.jsx
+++ src/commonComponents/Portrait.jsx
@@ -3,7 +3,7 @@ import landingImg from "@assets/portraits/landing.png";
 export default function Portrait() {
   return (
     <img
-      className="portrait hidden object-contain sepia-[.10] sm:block"
+      className="portrait red-filter hidden object-contain sm:block"
       src={landingImg}
       alt="landing image"
     />
