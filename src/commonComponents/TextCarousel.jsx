dd182a0069d71b1d9238117bb19d0cb94eb0ccb2
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-03-19 12:30:35 +0000

    refactor: remove colorGlitch stuff
    turns out I wasn't a fan of it :(

--- src/commonComponents/TextCarousel.jsx
+++ src/commonComponents/TextCarousel.jsx
@@ -2,23 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function TextCarousel({ inputTextArray }) {
   const [activeItemIndex, setActiveItem] = useState(0);
-  const [glitch, setGlitch] = useState(false);
-  const [glitchIndex, setGlitchIndex] = useState(0);
 
   const activeText = inputTextArray[activeItemIndex];
-
-  useEffect(() => {
-    function randGlitchIdx() {
-      setGlitchIndex(Math.floor(Math.random() * activeText.length));
-    }
-
-    if (glitch) {
-      const interval = setInterval(randGlitchIdx, 50);
-      return () => {
-        clearInterval(interval);
-      };
-    }
-  }, [glitchIndex, glitch]);
 
   useEffect(() => {
     function nextItem() {
@@ -31,15 +16,6 @@ export default function TextCarousel({ inputTextArray }) {
       clearInterval(interval);
     };
   }, [activeItemIndex, inputTextArray]);
-
-  useEffect(() => {
-    const countDown = glitch ? 1500 : 13500;
-    const timeout = setTimeout(() => setGlitch(!glitch), countDown);
-
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [glitch]);
 
   return (
     <div className="text-carousel flex overflow-hidden">
@@ -55,7 +31,6 @@ export default function TextCarousel({ inputTextArray }) {
           {input.split("").map((char, idx) => (
             <span
               key={`${activeText}-${idx}`}
-              className={`${glitch && idx === glitchIndex && randColor()}`}
             >
               {char}
             </span>
@@ -64,30 +39,4 @@ export default function TextCarousel({ inputTextArray }) {
       ))}
     </div>
   );
-
-  function randColor() {
-    const glitchColors = [
-      "text-slate-400",
-      "text-red-400",
-      "text-orange-400",
-      "text-amber-400",
-      "text-yellow-400",
-      "text-lime-400",
-      "text-green-400",
-      "text-emerald-400",
-      "text-teal-400",
-      "text-cyan-400",
-      "text-sky-400",
-      "text-blue-400",
-      "text-indigo-400",
-      "text-violet-400",
-      "text-purple-400",
-      "text-fuchsia-400",
-      "text-pink-400",
-      "text-rose-400",
-    ];
-
-    const randNum = Math.floor(Math.random() * glitchColors.length);
-    return glitchColors[randNum];
-  }
 }
