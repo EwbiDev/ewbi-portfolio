0b4ac6062484c61da01d37db3b83c215f85c5963
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-10 10:50:19 +0100

    feat:

---
+++ src/components/About/index.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <section className="m-4 flex h-screen snap-center flex-col justify-center">
+      <h2 className="text-5xl" id="About">
+        About Me
+      </h2>
+      <p>Hello this is an about me.</p>
+    </section>
+  );
+}
--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,10 +1,13 @@
 import NextPage from "@commonComponents/NextPage";
+
+import About from "../About";
 import Landing from "../Landing";
 
 export default function Content() {
   return (
     <main className="grow">
       <Landing />
+      <About />
       <NextPage />
     </main>
   );
