96f91a14152e78b70cb144708708821d58e457e5
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-15 15:56:31 +0100

    feat: add select none

--- src/components/Header/components/Logo.jsx
+++ src/components/Header/components/Logo.jsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <div className="logo bg-ewbi-red p-4 pb-0 align-text-bottom font-cursive text-8xl text-ewbi-gold">
+    <div className="logo select-none bg-ewbi-red p-4 pb-0 align-text-bottom font-cursive text-8xl text-ewbi-gold">
       <a href="#">Ewbi</a>
     </div>
   );
