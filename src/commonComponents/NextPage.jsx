3bb0e1f8b2973779f141860b0fd4e8f3af1092bd
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 17:13:53 +0100

    feat: raise nextPage widget

--- src/commonComponents/NextPage.jsx
+++ src/commonComponents/NextPage.jsx
@@ -1,6 +1,6 @@
 export default function NextPage({ nextSection }) {
   return (
-    <div className="material-symbols-outlined fixed bottom-4 right-1/2 -mr-5 -translate-y-1/4 cursor-pointer text-4xl will-change-transform hover:animate-bounce">
+    <div className="material-symbols-outlined fixed bottom-12 right-1/2 -mr-5 -translate-y-1/4 cursor-pointer text-4xl will-change-transform hover:animate-bounce">
       <a href={`#${nextSection}`}>keyboard_arrow_down</a>
     </div>
   );
