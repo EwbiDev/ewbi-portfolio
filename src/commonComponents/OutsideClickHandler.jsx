4d0d41d6a99170f56276c9bfa1dc5a1425e91533
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 19:32:44 +0100

    feat: add outSideClickHandler component

---
+++ src/commonComponents/OutsideClickHandler.jsx
@@ -0,0 +1,33 @@
+import { createRef, useEffect } from "react";
+
+export default function OutSideClickHandler({
+  onOutSideClick,
+  childSelector,
+  children,
+}) {
+  const ref = createRef();
+
+  useEffect(() => {
+    const elements = document.querySelectorAll(childSelector);
+
+    console.log(elements);
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+
+    function handleOutsideClick(event) {
+      const elementArray = Array.from(elements);
+      if (elementArray.every((element) => !element.contains(event.target))) {
+        onOutSideClick();
+      }
+    }
+  });
+
+  return (
+    <div className="hello h-min w-min" ref={ref}>
+      {children}
+    </div>
+  );
+}
