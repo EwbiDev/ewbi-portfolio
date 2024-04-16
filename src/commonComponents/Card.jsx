7088c549ac6a55d697357a0a35a028d4186f3914
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 12:08:07 +0100

    feat: improve card sizing

--- src/commonComponents/Card.jsx
+++ src/commonComponents/Card.jsx
@@ -1,3 +1,5 @@
 export default function Card({ children }) {
-  return <div className="rounded-md bg-white p-2 shadow-md">{children}</div>;
+  return (
+    <div className="w-fit rounded-md bg-white p-2 shadow-md">{children}</div>
+  );
 }
