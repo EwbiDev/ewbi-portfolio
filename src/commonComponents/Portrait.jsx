24d39b5f1321d814b509fecb3394ae7405da5ee6
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 19:35:59 +0100

    feat: amend portrait sizing

--- src/commonComponents/Portrait.jsx
+++ src/commonComponents/Portrait.jsx
@@ -3,7 +3,7 @@ import landingImg from "@assets/portraits/landing.png";
 export default function Portrait() {
   return (
     <img
-      className="portrait red-filter fixed bottom-0 right-0 z-10 block w-32 object-contain xl:w-96"
+      className="portrait red-filter fixed bottom-0 right-0 z-10 block w-48 object-contain lg:w-96"
       src={landingImg}
       alt="landing image"
     />
