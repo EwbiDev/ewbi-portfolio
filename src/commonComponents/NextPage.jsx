a56f06d5f4fc0325129c2771e43f50f8109c04da
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 12:31:28 +0100

    feat: center next page button

--- src/commonComponents/NextPage.jsx
+++ src/commonComponents/NextPage.jsx
@@ -1,6 +1,6 @@
 export default function NextPage() {
   return (
-    <div className="material-symbols-outlined fixed bottom-4 right-1/2 -translate-y-1/4 cursor-pointer text-4xl hover:animate-bounce">
+    <div className="material-symbols-outlined fixed bottom-4 right-1/2 -mr-5 -translate-y-1/4 cursor-pointer text-4xl will-change-transform hover:animate-bounce">
       keyboard_arrow_down
     </div>
   );
