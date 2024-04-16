320cf249cb1acc831ce14cf54904ca12bd3a215d
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 16:36:10 +0100

    style: formatting

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -15,23 +15,23 @@ export default function Contact() {
         <p>Feel free to get in touch in the following places:</p>
       </Card>
 
-        <ul className="flex flex-wrap gap-4">
-          <ContactLink
-            img={email}
-            text="Email"
-            url="mailto:eduard@ewbi.co.uk?subject=Website%20Enquiry"
-          />
-          <ContactLink
-            img={linkedin}
-            text="LinkedIn"
-            url="https://www.linkedin.com/in/eduard-bissell-6a7846194/"
-          />
-          <ContactLink
-            img={github}
-            text="GitHub"
-            url="https://www.github.com/EwbiDev"
-          />
-        </ul>
+      <ul className="flex flex-wrap gap-4">
+        <ContactLink
+          img={email}
+          text="Email"
+          url="mailto:eduard@ewbi.co.uk?subject=Website%20Enquiry"
+        />
+        <ContactLink
+          img={linkedin}
+          text="LinkedIn"
+          url="https://www.linkedin.com/in/eduard-bissell-6a7846194/"
+        />
+        <ContactLink
+          img={github}
+          text="GitHub"
+          url="https://www.github.com/EwbiDev"
+        />
+      </ul>
 
     </section>
   );
