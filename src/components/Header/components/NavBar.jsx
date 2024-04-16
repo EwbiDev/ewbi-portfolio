e46285151824d16ad58d9966ad55fcd32dc4c1ca
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-04-16 19:32:52 +0100

    feat: add burger menu

--- src/components/Header/components/NavBar.jsx
+++ src/components/Header/components/NavBar.jsx
@@ -1,14 +1,61 @@
+import OutSideClickHandler from "@commonComponents/OutsideClickHandler";
+import { useState } from "react";
+
 export default function NavBar() {
-  const navItems = ["About", "Projects", "Contact"];
   return (
     <nav className="flex items-center">
-      <ul className="flex gap-4">
-        {navItems.map((item) => (
-          <li key={`nav-item-${item}`} className="p-4 text-3xl text-ewbi-gold">
-            <a href={`#${item.toLowerCase()}`}>{item}</a>
-          </li>
-        ))}
-      </ul>
+      <NavItems mobileView={false} setBurgerClicked={() => {}} />
+      <Burger />
     </nav>
+  );
+}
+
+function NavItems({ mobileView, setBurgerClicked }) {
+  const navItems = ["About", "Projects", "Contact"];
+
+  const mobileClass =
+    "burger-dropdown flex flex-col sm:hidden top-24 right-0 bg-ewbi-red absolute";
+  const desktopClass = "hidden gap-2 sm:flex";
+
+  return (
+    <ul className={mobileView ? mobileClass : desktopClass}>
+      {navItems.map((item) => (
+        <li
+          key={`nav-item-${item}`}
+          className="flex text-3xl text-ewbi-gold hover:bg-ewbi-gold hover:text-ewbi-red"
+        >
+          <a
+            href={`#${item.toLowerCase()}`}
+            onClick={() => setBurgerClicked(false)}
+            className="p-4"
+          >
+            {item}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Burger({ children }) {
+  const [burgerClicked, setBurgerClicked] = useState(false);
+
+  return (
+    <>
+      <OutSideClickHandler
+        onOutSideClick={() => setBurgerClicked(false)}
+        childSelector=".burger-dropdown"
+      >
+        <div
+          className="burger-dropdown material-symbols-outlined cursor-pointer p-4 text-5xl text-ewbi-gold hover:bg-ewbi-gold hover:text-ewbi-red sm:hidden"
+          onClick={() => setBurgerClicked(!burgerClicked)}
+        >
+          Menu
+        </div>
+        {burgerClicked && (
+          <NavItems mobileView={true} setBurgerClicked={setBurgerClicked} />
+        )}
+      </OutSideClickHandler>
+    </>
   );
 }
