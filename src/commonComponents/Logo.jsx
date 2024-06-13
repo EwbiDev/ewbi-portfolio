b5d436b13e34e9e0445b1b7d8d18adc29ce142e5
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-06-13 17:53:15 +0100

    feat: add option to position tooltip to the right

--- src/commonComponents/Logo.jsx
+++ src/commonComponents/Logo.jsx
@@ -1,12 +1,20 @@
-export default function Logo({ url, name }) {
-    const tooltipClass =
-      "absolute left-1/2 m-4 mx-auto -translate-x-1/2 translate-y-8 rounded-md bg-ewbi-red py-1 px-2 text-sm text-ewbi-gold opacity-0 transition-opacity group-hover:opacity-100 w-max";
-    return (
-      <li>
-        <div className="group relative flex">
-          <img src={url} className="w-10 transition hover:scale-125"></img>
-          <div className={tooltipClass}>{name}</div>
+export default function Logo({ url, name, positionTooltipRight }) {
+  const tooltipBottomClass =
+    "absolute left-1/2 mx-auto -translate-x-1/2 translate-y-12 rounded-md bg-ewbi-red py-1 px-2 text-sm text-ewbi-gold opacity-0 transition-opacity group-hover:opacity-100 w-max";
+  const tooltipRightClass =
+    tooltipBottomClass + " md:left-12 md:translate-y-1/4 md:-translate-x-0";
+  return (
+    <li>
+      <div className="group relative flex">
+        <img src={url} className="w-10 transition hover:scale-125"></img>
+        <div
+          className={
+            positionTooltipRight ? tooltipRightClass : tooltipBottomClass
+          }
+        >
+          {name}
         </div>
-      </li>
-    );
-  }
+      </div>
+    </li>
+  );
+}
